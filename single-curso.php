6acd028d3866878ab568ce7f82c9d79ed3364ae7
Kleber Alves <seu@hotmail.com>
2019-10-06 16:45:43 -0300

    criando o arquivo single para cursos

---
+++ single-curso.php
@@ -0,0 +1,48 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ts</title>
+	<link rel="stylesheet" type="text/css" href="https://www.perfilempregos.com.br/wp-content/themes/perfilempregos/assets/css/theme.css?ver=4.2.24"> 
+<style type="text/css">
+	
+	img {width:10px;}
+</style>
+
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+      <main id="main" class="col-md-8">
+        <div class="main-curso">
+<h2>conteudo</h2> 
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor.
+       </div>
+
+      </main><!-- #main -->
+      <aside class="col-md-4">
+        <div class="main-aside">
+          <h4>Empreendedorismo   <?php the_title(); ?></h4>
+          <img src="http://japainformatica.com/wp-content/uploads/2018/06/EMPREENDEDORISMO-DE-ALTA-PERFORMANCE-768x481-1.jpg" class="side-image"> the_image()
+          <div class="desc"> the_excerpt()
+            <p><b>O QUE EU VOU APRENDER?</b></p>
+<p>Neste curso você irá aprender quais as características fundamentais para ser um empreendedor de sucesso, desenvolvendo um ciclo de prosperidade para você e para seu negócio.<br>
+<b></b></p>.... 
+          </div>
+          <p><i class="far fa-clock"></i> Carga horária: 60hcarga_horarioa</p>
+          <p><i class="fas fa-graduation-cap"></i> Certificado de conclusão</p>
+		  <span class="valor-de">De 597,00</span>
+          <span class="valor">Valor R$297,00</span>
+          <!--<a href="#" class="bt-comprar"><i class="fa fa-shopping-cart"></i> Comprar agora</a>-->
+          <form method="post" action="">		<input name="item_id" type="hidden" value="1">
+		<input name="item_descricao" type="hidden" value="Empreendedorismo">
+		<input name="item_qtd" type="hidden" value="1">
+		<input name="item_valor" type="hidden" value="297,00">
+		<input name="submit" type="submit" value="Comprar agora" class="bt-comprar">
+	</form>
+	        </div>
+      </aside>
+    </div>
+</body>
+</html>
